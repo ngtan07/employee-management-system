206c628d1fead17ae6acd470fd5d9f59e76cfa19
ngtan07 <ngthanhan0712@gmail.com>
2026-02-16 02:40:42 +0700

    Initial commit

---
+++ esm-backend/src/main/java/net/javaguides/esm/EsmBackendApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsmBackendApplication.class, args);
+	}
+
+}
