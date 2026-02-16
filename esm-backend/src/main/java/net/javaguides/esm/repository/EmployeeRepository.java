b486fe03d90bf13b79d6598dbd08b02e3f8b227a
ngtan07 <ngthanhan0712@gmail.com>
2026-02-17 00:16:55 +0700

    feat: create employee

---
+++ esm-backend/src/main/java/net/javaguides/esm/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package net.javaguides.esm.repository;
+
+import net.javaguides.esm.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ esm-backend/src/main/java/net/javaguides/esm/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package net.javaguides.esm.service;
+
+import net.javaguides.esm.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
