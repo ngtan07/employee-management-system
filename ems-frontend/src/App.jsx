ce5de45daee2806d8ae82e62212d58e1f877a084
ngtan07 <ngthanhan0712@gmail.com>
2026-02-18 20:24:46 +0700

    chore: config routing

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <ListEmployeeComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>
+
+        <FooterComponent />
+      </BrowserRouter>
+
     </>
   )
 }
---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className='footer'>
+                <span>All rights reserved 2026 by Ng T An</span>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent
